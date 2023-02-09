b0113c0fb307a53442ea1cd7d87dcd134de45199
gunawansetia <newgunawansetiawiguna@gmail.com>
2023-02-09 21:20:11 +0700

    perubahan

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 hitungLuas = (p, l) => {
   return p * l;
 };
- 
+
 // edited
+<<<<<<< Updated upstream
+=======
+// edited kedua
+// edited ketiga
+// edited keempat
+>>>>>>> Stashed changes
 
 console.log(hitungLuas(2, 5));
